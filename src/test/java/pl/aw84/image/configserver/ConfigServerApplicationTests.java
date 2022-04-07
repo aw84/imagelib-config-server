9a8f35c069b04adfe15611babb0422aa30e3121e
aw84 <silencewalker@gmail.com>
2022-04-07 21:03:41 +0200

    init

---
+++ src/test/java/pl/aw84/image/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.aw84.image.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
